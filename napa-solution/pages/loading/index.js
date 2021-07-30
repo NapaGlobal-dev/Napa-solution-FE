87ee90cca914a40f2407be2d6741bc7dc33ad437
thuong <thuongzzu@gmail.com>
2021-07-30 09:46:43 +0700

    fix ui landing page and create loading screen v2

---
+++ napa-solution/pages/loading/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import React from 'react';
+
+const loading = () => {
+    const textSlide = ["N", "A", "P", "A", "..."];
+    return (
+        <>
+            <Head>
+            <link key="css/loading.css" rel="stylesheet" href="css/loading.css" />
+            </Head>
+            {/* <div className="content">
+                <div >
+                    <div className="loading-page">
+                        <div className="spinner">
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+
+            {/* loading v2 */}
+            <div className = "wrap-loader">
+                <div className = "loader">
+                    {new Array(4).fill(null).map((item, key) =>(
+                        <div className= "box" key = {key} />
+                    ))}
+                    <div className = "wrap-text">
+                        <div className = "text">
+                            {textSlide.map((item, key) =>(
+                                <span key = {key}>{item}</span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className = "loader-text">
+
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default loading
