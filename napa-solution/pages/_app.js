84df001e388b28ca5a339cee9e4f37b508e016f3
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2021-08-02 14:42:27 +0700

    dark-mode

--- napa-solution/pages/_app.js
+++ napa-solution/pages/_app.js
@@ -5,17 +5,34 @@ import App from "next/app";
 
 import { client } from "../apolo-client";
 import { footerDataQuery } from "../query/general";
-import '../styles/globals.css';
+import "../styles/globals.css";
 import StoreProvier, { StoreContext } from "../util/language/store";
-import { useContext } from "react";
+import { useState, useEffect } from "react";
 
+import useDarkMode from "use-dark-mode";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../themeConfig";
 function MyApp({ Component, pageProps, footerData, ...props }) {
+  const [isMounted, setIsMounted] = useState(false);
+  const darkmode = useDarkMode(true);
+  const theme = darkmode.value ? darkTheme : lightTheme;
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
   return (
     <StoreProvier>
       <ApolloProvider client={client}>
-        <Layout footerData={footerData}>
-          <Component {...pageProps} />
-        </Layout>
+        <ThemeProvider theme={theme}>
+          <Layout footerData={footerData}>
+            <button
+              onClick={darkmode.toggle}
+              style={{ position: "absolute", top: 30, zIndex: 20 }}
+            >
+              Switch Mode
+            </button>
+            {isMounted && <Component {...pageProps} />}
+          </Layout>
+        </ThemeProvider>
       </ApolloProvider>
     </StoreProvier>
   );
