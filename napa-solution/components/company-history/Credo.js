0fc7946d91f89eaa49d48c4d7ed8d6f8621f703a
DungxDang <dung.ddang@gmail.com>
2021-08-12 15:12:30 +0700

    history style

--- napa-solution/components/company-history/Credo.js
+++ napa-solution/components/company-history/Credo.js
@@ -3,49 +3,93 @@ import { getData } from "../../util/converArrayToObject"
 export default function Credo({data}){
     // const history = getData(data, /CompanyHistory_History/)
     return(
-        <div className='containerD'>
-            <h1 className='titleD'>Credo</h1>
-            <div className='subtitleD'>クレド</div>
-            <div className='rowD'>
-                <div className='leftColumn'>1. Win-Winの原則</div>
-                <div className='rightColumn'>私たちは、事業に関わる人すべてに幸福と成功をもたらすようベストを尽くします</div>
+        <>
+            <div className='cover'>
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="58.948"
+                height="124.343"
+                viewBox="0 0 58.948 124.343"
+                >
+                <g
+                    id="Group_135"
+                    data-name="Group 135"
+                    transform="translate(-98.543 -1182.829)"
+                >
+                    <line
+                    id="Line_88"
+                    data-name="Line 88"
+                    x1="94.203"
+                    transform="matrix(0.574, -0.819, 0.819, 0.574, 101, 1261.142)"
+                    fill="none"
+                    stroke="#6a43d5"
+                    stroke-width="4"
+                    />
+                    <line
+                    id="Line_89"
+                    data-name="Line 89"
+                    x1="94.203"
+                    transform="matrix(0.574, -0.819, 0.819, 0.574, 101, 1283.819)"
+                    fill="none"
+                    stroke="#6a43d5"
+                    stroke-width="6"
+                    />
+                    <line
+                    id="Line_90"
+                    data-name="Line 90"
+                    x1="94.203"
+                    transform="matrix(0.574, -0.819, 0.819, 0.574, 101, 1306.024)"
+                    fill="none"
+                    stroke="#6a43d5"
+                    stroke-width="4"
+                    />
+                </g>
+                </svg>
+                <h3>Credo</h3>
+                <p>クレド</p>
             </div>
-            <div className='rowD'>
-                <div className='leftColumn'>2. 先取りの精神</div>
-                <div className='rightColumn'>私たちは、新しいことに挑戦しつづける精神を持ち続けます</div>
+            <div className='containerD'>
+                <div className='rowD'>
+                    <div className='leftColumn'>1. Win-Winの原則</div>
+                    <div className='rightColumn'>私たちは、事業に関わる人すべてに幸福と成功をもたらすようベストを尽くします</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>2. 先取りの精神</div>
+                    <div className='rightColumn'>私たちは、新しいことに挑戦しつづける精神を持ち続けます</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>3. 貢献へのフォーカス</div>
+                    <div className='rightColumn'>私たちは、すべての仕事の意味と成果を、社会への貢献と結びつけます</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>4. 強みへのフォーカス</div>
+                    <div className='rightColumn'>私たちは、自らの強みを活かせる分野に特化します</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>5. 自律と自立</div>
+                    <div className='rightColumn'>私たちは、メンバー個々人が自律的・自立的に行動し、職務を全うします</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>6. 他者への尊敬</div>
+                    <div className='rightColumn'>私たちは、メンバー個々人の価値観と存在を理解・尊重し、互いの成長を喜びます</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>7. 社会的存在</div>
+                    <div className='rightColumn'>私たちは、誠実な会社であると社会から信頼され尊敬される存在でありつづけます</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>8. 成果主義</div>
+                    <div className='rightColumn'>私たちは、貢献と成果に応じて自らを評価します</div>
+                </div>
+                <div className='rowD'>
+                    <div className='leftColumn'>9. 妥協しない</div>
+                    <div className='rightColumn'>私たちは、常に、考えうる最高の品質をご提供します</div>
+                </div>
+                <div className='rowD-end'>
+                    <div className='leftColumn'>10. できないではなく、やり方を考える</div>
+                    <div className='rightColumn'>私たちは、できないではなく、やり方を考えます。また、より良い方法を模索しつ づけます</div>
+                </div>
             </div>
-            <div className='rowD'>
-                <div className='leftColumn'>3. 貢献へのフォーカス</div>
-                <div className='rightColumn'>私たちは、すべての仕事の意味と成果を、社会への貢献と結びつけます</div>
-            </div>
-            <div className='rowD'>
-                <div className='leftColumn'>4. 強みへのフォーカス</div>
-                <div className='rightColumn'>私たちは、自らの強みを活かせる分野に特化します</div>
-            </div>
-            <div className='rowD'>
-                <div className='leftColumn'>5. 自律と自立</div>
-                <div className='rightColumn'>私たちは、メンバー個々人が自律的・自立的に行動し、職務を全うします</div>
-            </div>
-            <div className='rowD'>
-                <div className='leftColumn'>6. 他者への尊敬</div>
-                <div className='rightColumn'>私たちは、メンバー個々人の価値観と存在を理解・尊重し、互いの成長を喜びます</div>
-            </div>
-            <div className='rowD'>
-                <div className='leftColumn'>7. 社会的存在</div>
-                <div className='rightColumn'>私たちは、誠実な会社であると社会から信頼され尊敬される存在でありつづけます</div>
-            </div>
-            <div className='rowD'>
-                <div className='leftColumn'>8. 成果主義</div>
-                <div className='rightColumn'>私たちは、貢献と成果に応じて自らを評価します</div>
-            </div>
-            <div className='rowD'>
-                <div className='leftColumn'>9. 妥協しない</div>
-                <div className='rightColumn'>私たちは、常に、考えうる最高の品質をご提供します</div>
-            </div>
-            <div className='rowD-end'>
-                <div className='leftColumn'>10. できないではなく、やり方を考える</div>
-                <div className='rightColumn'>私たちは、できないではなく、やり方を考えます。また、より良い方法を模索しつ づけます</div>
-            </div>
-        </div>
+        </>
     )
 }
