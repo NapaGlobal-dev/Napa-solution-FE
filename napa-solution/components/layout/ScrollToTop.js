fcf4462c78d09f4b58d93619e5b176800e249b49
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2021-08-13 11:47:00 +0700

    done

--- napa-solution/components/layout/ScrollToTop.js
+++ napa-solution/components/layout/ScrollToTop.js
@@ -1,19 +1,53 @@
 import React, { useEffect, useState } from "react";
-
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = useState(false);
 
   // Top: 0 takes us all the way back to the top of the page
   // Behavior: smooth keeps it smooth!
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    scrollTo(0, 2468);
+
+    // Element to move, time in ms to animate
+    function scrollTo(element, duration) {
+      var e = document.documentElement;
+      if (e.scrollTop === 0) {
+        var t = e.scrollTop;
+        ++e.scrollTop;
+        e = t + 1 === e.scrollTop-- ? e : document.body;
+      }
+      scrollToC(e, e.scrollTop, element, duration);
+    }
+
+    // Element to move, element or px from, element or px to, time in ms to animate
+    function scrollToC(element, from, to, duration) {
+      if (duration <= 0) return;
+      if (typeof from === "object") from = from.offsetTop;
+      if (typeof to === "object") to = to.offsetTop;
+
+      scrollToX(element, from, to, 0.55, 1 / duration, 30, easeOutCuaic);
+    }
+
+    function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+      if (t01 < 0 || t01 > 1 || speed <= 0) {
+        element.scrollTop = xTo;
+        return;
+      }
+      element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+      t01 += speed * step;
+
+      setTimeout(function () {
+        scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+      }, step);
+    }
+
+    function easeOutCuaic(t) {
+      t--;
+      return t * t * t + 1;
+    }
   };
 
   useEffect(() => {
-    // Button is displayed after scrolling for 500 pixels
+    // Button is displayed after scrolling for 10 pixels
     const toggleVisibility = () => {
       if (window.pageYOffset > 10) {
         setIsVisible(true);
@@ -21,7 +55,6 @@ export default function ScrollToTop() {
         setIsVisible(false);
       }
     };
-
     window.addEventListener("scroll", toggleVisibility);
 
     return () => window.removeEventListener("scroll", toggleVisibility);
@@ -57,29 +90,25 @@ export default function ScrollToTop() {
       opacity:0.7;
   }   
     </style>`);
-    // $(".scroll_to_topp").hover(() => {
-    //   $(this).css("transform", "translateX(0px) !important");
-    // });
   });
-  //scroll-to-top classes: fixed, bottom:0, right:0
+
   return (
     <>
       {isVisible && (
         <div className="scroll_to_top" id="scrollToTop" onClick={scrollToTop}>
           <img
             src="./img/scroll-top.png"
-            style={{ height: 30, width: "auto", marginRight: 12 }}
-          />
-          {/* <h3
             style={{
-              fontSize: 25,
-              borderLeft: "1px solid #FFF",
-              paddingLeft: 12,
-              height: "100%",
+              height: 30,
+              width: "auto",
+              marginRight: 12,
+              outline: "none",
+              webkitUserSelect: "none",
+              mozUserSelect: "none",
+              msUserSelect: "none",
+              userSelect: "none",
             }}
-          >
-            TOP
-          </h3> */}
+          />
         </div>
       )}
     </>
