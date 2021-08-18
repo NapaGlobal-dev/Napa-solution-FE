0d3863993cd2f1cdc81a5533201d63967a538796
thuong <thuongzzu@gmail.com>
2021-08-18 10:17:17 +0700

    merge master

--- napa-solution/components/offshore/Banner.js
+++ napa-solution/components/offshore/Banner.js
@@ -1,36 +1,41 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import { convertArrToObject } from "../../util/converArrayToObject";
 const Banner = (props) => {
-  const banner = convertArrToObject(props.data.property)
+  const data = convertArrToObject(props.data.property);
+  useEffect(() => {
+    window.onload = function () {
+      document.getElementById("banner").className = "wrap-banner open";
+    };
+  }, []);
   return (
     <>
       <Head>
         <link key="css/banner.css" rel="stylesheet" href="css/banner.css" />
       </Head>
       <div className="banner">
-        {/* <LazyLoadImage
-                    effect='blur'
-                    src={banner?.image?.original}
-                    placeholderSrc={banner?.image?.thumbnail}
-                    threshold={100}
-                    height='100%'
-                    width='100%'
-                    className="image-header"
-                /> */}
         <div className="overlay-headerA"></div>
-        <img src="/img/untitled-img/contact.jpeg" className="imageA" alt="" />
-        <div className="border-titleA">
-          <h1 style={{ letterSpacing: -3, fontSize: "3.3rem" }}>Offshore</h1>
+        <img
+          src={data["Offshore_Banner_Img"].image.original}
+          className="imageA"
+          alt=""
+        />
+        <div className="wrap-banner" id="banner">
+          <h1 className="main-title">
+            {data["Offshore_Banner_Title"].value}
+          </h1>
           <div style={{ marginTop: 20, fontSize: "1.2rem" }}>
-            {banner.Offshore_Banner_Title.value}
+            {data["Offshore_Banner_Subtitle"].value}
           </div>
+          <div className="frame-tb"></div>
+          <div className="frame-lr"></div>
         </div>
         <div className="textB">
           <div className="titleB">
-            ブルー・マーリン・パートナーズに、ご興味をお持ちいただきありがとうございます。
+            {data["Offshore_Banner_Content"].value}
           </div>
           <div className="subtitleB">
-            下記受付フォームに必要項目を入力して送信してください。3営業日以内に担当者より折り返しご連絡させていただきます。
+            {data["Offshore_Banner_Subcontent"].value}
           </div>
         </div>
       </div>
