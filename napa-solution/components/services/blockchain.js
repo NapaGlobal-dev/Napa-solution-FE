e142170c206f4d7db32e17017201fd4fdad3506c
DungxDang <dung.ddang@gmail.com>
2021-08-19 13:36:57 +0700

    apply animate to services

--- napa-solution/components/services/blockchain.js
+++ napa-solution/components/services/blockchain.js
@@ -10,8 +10,8 @@ const BlockChain = (props) => {
       <div className="sv-cover-padding">
         <div className={clsx("sv-cover")}>
           <img className="sv-decor-head-line" src="/img/line-style.svg" />
-          <h3>{data.BlockChain_TitleEN.value}</h3>
-          <p>{data.BlockChain_TitleJP.value}</p>
+          <h3 id='down-up'>{data.BlockChain_TitleEN.value}</h3>
+          <p id='down-up'>{data.BlockChain_TitleJP.value}</p>
           <hr className="sv-horizontal-bar" />
           <div className="sv-img-bouding">
             <picture>
@@ -25,7 +25,7 @@ const BlockChain = (props) => {
               />
             </picture>
             <div className="sv-box">
-              <div className="sv-content">
+              <div className="sv-content" id='down-up'>
                 {joinJsx(
                   data.BlockChain_Content.value.split("\\n"),
                   <br />
