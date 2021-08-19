b64661cdd80a227247338a03a7dbb50bebdbbb09
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2021-08-19 15:04:02 +0700

    fix header

--- napa-solution/components/company/Banner.js
+++ napa-solution/components/company/Banner.js
@@ -20,7 +20,7 @@ const Banner = (props) => {
         />
         <div className="wrap-banner" id="banner">
           <h1 className="main-title">{data["CompanyProfile_Banner_Title"].value}</h1>
-          <div style={{ marginTop: 20, fontSize: "1.2rem" }}>
+          <div className="sub-title">
             {data["CompanyProfile_Banner_SubTitle"].value}
           </div>
           <div className="frame-tb"></div>
