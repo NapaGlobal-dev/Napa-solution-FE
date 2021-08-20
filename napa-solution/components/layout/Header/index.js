dfa8403e2bbb23b3b55adfeb4ac1573f19276f5d
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2021-08-20 15:55:36 +0700

    refactor-footer

--- napa-solution/components/layout/Header/index.js
+++ napa-solution/components/layout/Header/index.js
@@ -16,10 +16,11 @@ function Language() {
   const [openDropdown, setOpenDropndown] = useState(false);
   const [language, setLanguage] = useState({});
   const dataLang = useContext(StoreContext)?.language;
+
   useEffect(() => {
     setLanguage(dataLang);
   }, []);
-  
+
   return (
     <div className="langWrapper">
       <div className="langBtn" onClick={() => setOpenDropndown(!openDropdown)}>
@@ -39,7 +40,9 @@ function Language() {
             }}
             className={index === (language && language[0]) ? "lang-active" : ""}
           >
-            <a href = {lang !== "JP" ? "http://www.napaglobal.com" : "#"} >{lang}</a>
+            <a href={lang !== "JP" ? "http://www.napaglobal.com" : "#"}>
+              {lang}
+            </a>
           </div>
         ))}
       </div>
@@ -55,13 +58,11 @@ const Header = (props) => {
   const navbarMobile = getData(data, /Navbar_Menu/);
   const navRef = useRef(null);
   // console.log("navbarsss", navbarMenu, data);
-  const darkmode = useDarkMode()
+  const darkmode = useDarkMode();
   useEffect(() => {
-    const hour = new Date().getHours()
-    if (hour < 5 || hour >= 19)
-      darkmode.enable()
-    else
-      darkmode.disable()
+    const hour = new Date().getHours();
+    if (hour < 5 || hour >= 19) darkmode.enable();
+    else darkmode.disable();
   }, []);
 
   // const [changeNav, setChangeNav] = useState(true);
@@ -210,7 +211,7 @@ const Header = (props) => {
                     </a>
                   </div>
                 </div>
-                {(navbarHome && navbarHome.content.length !== 0) && (
+                {navbarHome && navbarHome.content.length !== 0 && (
                   <div className="dropdown-layer">
                     <div className="dropdown-body">
                       <ul>
