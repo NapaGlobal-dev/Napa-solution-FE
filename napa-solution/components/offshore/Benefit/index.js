396a652119fd05e2fa4b5925db3e559275f9e17a
hungphannapaglobal <hung.phan@napaglobal.com>
2021-08-25 12:23:54 +0700

    update CSS

--- napa-solution/components/offshore/Benefit/index.js
+++ napa-solution/components/offshore/Benefit/index.js
@@ -12,62 +12,33 @@ const Benefit = (props) => {
   );
   const darkmode = useDarkMode();
   useEffect(() => {
-    if(darkmode?.value){
-      document.getElementsByClassName(styles.benefitDetail)[0].classList.add(styles.benefitDetailDarkMode)
-    }else{
-      document.getElementsByClassName(styles.benefitDetail)[0].classList.remove(styles.benefitDetailDarkMode)
+    if (darkmode?.value) {
+      document
+        .getElementsByClassName(styles.benefitDetail)[0]
+        .classList.add(styles.benefitDetailDarkMode);
+    } else {
+      document
+        .getElementsByClassName(styles.benefitDetail)[0]
+        .classList.remove(styles.benefitDetailDarkMode);
     }
-  })
+  });
   return (
     <div className="container-fluid">
       <div className={clsx("cover", styles.wrapContainer)}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="58.948"
-          height="124.343"
-          viewBox="0 0 58.948 124.343"
-        >
-          <g
-            id="Group_135"
-            data-name="Group 135"
-            transform="translate(-98.543 -1182.829)"
-          >
-            <line
-              id="Line_88"
-              data-name="Line 88"
-              x1="94.203"
-              transform="matrix(0.574, -0.819, 0.819, 0.574, 101, 1261.142)"
-              fill="none"
-              stroke="#6a43d5"
-              stroke-width="4"
-            />
-            <line
-              id="Line_89"
-              data-name="Line 89"
-              x1="94.203"
-              transform="matrix(0.574, -0.819, 0.819, 0.574, 101, 1283.819)"
-              fill="none"
-              stroke="#6a43d5"
-              stroke-width="6"
-            />
-            <line
-              id="Line_90"
-              data-name="Line 90"
-              x1="94.203"
-              transform="matrix(0.574, -0.819, 0.819, 0.574, 101, 1306.024)"
-              fill="none"
-              stroke="#6a43d5"
-              stroke-width="4"
-            />
-          </g>
-        </svg>
+        <img className="decor-head-line" src="/img/line-style.svg" />
         <h3 id="down-up">{benefit.OffShore_Benefit_Title.value}</h3>
         <p id="down-up">{benefit.OffShore_Benefit_Subtitle.value}</p>
         <div>
           <div className={styles.item}>
             <img src={benefit.OffShore_Benefit_Img.image.original} />
             <div className={styles.benefitBorder}>
-              <div className={clsx(styles.benefitDetail, darkmode?.value && styles.benefitDetailDarkMode)} id="down-up" >
+              <div
+                className={clsx(
+                  styles.benefitDetail,
+                  darkmode?.value && styles.benefitDetailDarkMode
+                )}
+                id="down-up"
+              >
                 {contentBenefit.map((item, index) => (
                   <div className={styles.wraperContent} key={index}>
                     <div className={styles.benefitNumber}>
