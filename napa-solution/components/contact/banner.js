0dfa0410c821e78cde069b3a98b05ee00b0b5a77
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2021-08-31 08:56:00 +0700

    new banner contact

--- napa-solution/components/contact/banner.js
+++ napa-solution/components/contact/banner.js
@@ -7,29 +7,19 @@ export default function Banner({ data }) {
   const subtitle = getData(data, /ContactBanner_Banner_SubTitle/)[0];
   const content = getData(data, /ContactBanner_Banner_Content/)[0];
   const subcontent = getData(data, /ContactBanner_Banner_SubContent/)[0];
-  useEffect(() => {
-    window.onload = function () {
-      document.getElementById("banner").className = "wrap-banner open";
-    };
-  }, []);
+  // useEffect(() => {
+  //   window.onload = function () {
+  //     document.getElementById("banner").className = "wrap-banner open";
+  //   };
+  // }, []);
   return (
     <>
       <Head>
         <link key="css/banner.css" rel="stylesheet" href="css/banner.css" />
       </Head>
-      <div className="banner">
-        {/* <LazyLoadImage
-                    effect='blur'
-                    src={banner?.image?.original}
-                    placeholderSrc={banner?.image?.thumbnail}
-                    threshold={100}
-                    height='100%'
-                    width='100%'
-                    className="image-header"
-                /> */}
-        <div className="overlay-header-banner"></div>
-        <img src={banner?.image?.original} className="imageA" alt="" />
-        <div className="wrap-banner" id="banner">
+      
+      <div className="banner-page banner">
+        <div className="title">
           <h1 className="main-title">
             {title?.key}
             {title?.value}
@@ -38,19 +28,10 @@ export default function Banner({ data }) {
             {subtitle?.key}
             {subtitle?.value}
           </div>
-          <div className="frame-tb"></div>
-          <div className="frame-lr"></div>
         </div>
-        {/* <div className="wrap-content-banner">
-          <div className="content-banner">
-            {content?.key}
-            {content?.value}
-          </div>
-          <div className="subcontent-banner">
-            {subcontent?.key}
-            {subcontent?.value}
-          </div>
-        </div> */}
+        <div className="img-banner">
+          <img src="/img/img-map.svg" />
+        </div>
       </div>
     </>
   );
