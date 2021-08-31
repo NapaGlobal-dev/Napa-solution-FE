aaf03bd3c334d27e234fa6d030527b84a8ffa735
sinhvienit17 <nguyentuanquangsang1999@gmail.com>
2021-08-31 15:31:14 +0700

    add-service-case-study

---
+++ napa-solution/components/case-study/service.js
@@ -0,0 +1,85 @@
+import Head from "next/head";
+import { convertArrToObject } from "../../util/converArrayToObject";
+export default function Service(props) {
+  const { data } = props;
+  const getValue = (content, name) => {
+    return Object.values(content).filter((it) => it.name.includes(name))[0]
+      .value;
+  };
+  const getImg = (content, name) => {
+    return Object.values(content).filter((it) => it.name.includes(name))[0]
+      .image.original;
+  };
+  return (
+    <>
+      <Head>
+        <link key="/css/banner.css" rel="stylesheet" href="/css/banner.css" />
+      </Head>
+      <div className="wrap-case sl-container">
+        {data.map((item, index) =>
+          index % 2 == 0 ? (
+            <div className="case-layout">
+              <div
+                className="case-img"
+                style={{
+                  background: `${getValue(
+                    item.content,
+                    "RealEstate_Service_Background"
+                  )}`,
+                }}
+              >
+                <img src={getImg(item.content, "RealEstate_Service_ImgCase")} />
+              </div>
+              <div className="case-item">
+                <div className="service-name">
+                  {getValue(item.content, "RealEstate_Service_ServiceName")}
+                </div>
+                <div className="case-name">
+                  {getValue(item.content, "RealEstate_Service_CaseName")}
+                </div>
+                <div className="case-content">
+                  {getValue(item.content, "RealEstate_Service_CaseContent")}
+                </div>
+                <div className="case-program">
+                  <img
+                    src={getImg(item.content, "RealEstate_Service_ImgProgram")}
+                  />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="case-layout left">
+              <div className="case-item">
+                <div className="service-name">
+                  {getValue(item.content, "RealEstate_Service_ServiceName")}
+                </div>
+                <div className="case-name">
+                  {getValue(item.content, "RealEstate_Service_CaseName")}
+                </div>
+                <div className="case-content">
+                  {getValue(item.content, "RealEstate_Service_CaseContent")}
+                </div>
+                <div className="case-program">
+                  <img
+                    src={getImg(item.content, "RealEstate_Service_ImgProgram")}
+                  />
+                </div>
+              </div>
+              <div
+                className="case-img"
+                style={{
+                  background: `${getValue(
+                    item.content,
+                    "RealEstate_Service_Background"
+                  )}`,
+                }}
+              >
+                <img src={getImg(item.content, "RealEstate_Service_ImgCase")} />
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </>
+  );
+}
