1adb072d9a36f52c1f2f7b3306e3310073c6e663
hungphannapaglobal <hung.phan@napaglobal.com>
2021-09-01 10:16:39 +0700

    remove slide

--- napa-solution/components/homepage/Service/index.js
+++ napa-solution/components/homepage/Service/index.js
@@ -1,14 +1,11 @@
 import styles from "./style.module.css";
 import clsx from "clsx";
-import { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const convertData = (data) => {
   const newdata = {};
   data.map((item) => {
     const [name, order] = item.name.split("_")[2].split("-");
-    // item.name.includes("Img", "title", "subTitle", "url", "btn");
 
     if (!newdata[order]) newdata[order] = {};
     newdata[order][name] = item;
@@ -31,28 +28,10 @@ const convertData = (data) => {
 
 const Service = (props) => {
   const data = convertData(props.data.property);
-  const [swiperRef, setSwiperRef] = useState(null);
-
-  useEffect(() => {
-    if (swiperRef) {
-      swiperRef.update();
-      swiperRef.updateProgress();
-      swiperRef.updateSize();
-      swiperRef.updateSlides();
-    }
-  }, [swiperRef, setSwiperRef]);
 
   return (
     <>
-      <Swiper
-        className="sw-container"
-        onSwiper={setSwiperRef}
-        direction={"vertical"}
-        slidesPerView={2}
-        mousewheel
-        slidesPerGroup={2}
-        nested
-      >
+      <div className={styles.serviceContainer}>
         <div
           className={clsx("container-fluid", styles.container)}
           style={{ display: "none" }}
@@ -65,95 +44,72 @@ const Service = (props) => {
           </div>
         </div>
         {data.map((item, index) => (
-          <SwiperSlide className="sw-slide-services " key={index}>
-            <div className="sw-box-bounding">
-              <div className="sw-box-content" key={index}>
-                {index % 2 == 0 && (
-                  <div className={styles.coverText}>
-                    <h2 className={clsx(styles.h2Text)}>SERVICES</h2>
-                  </div>
-                )}
-                <div
-                  className={
-                    // clsx("row", index % 2 != 0 && styles.reverse)
-                    index % 2 == 0
-                      ? clsx(styles.cover, styles.initial)
-                      : clsx(styles.cover, styles.reverse)
-                  }
-                >
-                  <div className={clsx(styles.blockContent)}>
-                    <div className={styles.blockImageBounding}>
-                      <div className={clsx(styles.blockImage)}>
-                        <LazyLoadImage
-                          effect="blur"
-                          src={item.img}
-                          placeholderSrc={item.imgThumbnail}
-                          threshold={100}
-                          className={clsx(styles.mainImage)}
-                        />
-                        <img
-                          src={
-                            index % 2 == 0
-                              ? "img/home/bg-service-left.png"
-                              : "img/home/bg_service.svg"
-                          }
-                          className={
-                            clsx(styles.decorImg)
-                            // index % 2 == 0
-                            //   ? clsx(styles.bgImgOdd)
-                            //   : clsx(styles.bgImgEven)
-                          }
-                        />
-                      </div>
+          <div key={index} className="sw-box-bounding">
+            <div className="sw-box-content" key={index}>
+              {index % 2 == 0 && (
+                <div className={styles.coverText}>
+                  <h2 className={clsx(styles.h2Text)}>SERVICES</h2>
+                </div>
+              )}
+              <div
+                className={
+                  index % 2 == 0
+                    ? clsx(styles.cover, styles.initial)
+                    : clsx(styles.cover, styles.reverse)
+                }
+              >
+                <div className={clsx(styles.blockContent)}>
+                  <div className={styles.blockImageBounding}>
+                    <div className={clsx(styles.blockImage)}>
+                      <LazyLoadImage
+                        effect="blur"
+                        src={item.img}
+                        placeholderSrc={item.imgThumbnail}
+                        threshold={100}
+                        className={clsx(styles.mainImage)}
+                      />
+                      <img
+                        src={
+                          index % 2 == 0
+                            ? "img/home/bg-service-left.png"
+                            : "img/home/bg_service.svg"
+                        }
+                        className={clsx(styles.decorImg)}
+                      />
                     </div>
                   </div>
-                  <div className={clsx(styles.spacing)}></div>
+                </div>
+                <div className={clsx(styles.spacing)}></div>
 
-                  <div
-                    className={
-                      clsx(styles.blockContent)
-                      // index % 2 == 0
-                      //   ? clsx(styles.contentText)
-                      //   : clsx(styles.contentReverse)
-                    }
-                  >
-                    <div className={clsx(styles.centerContent)}>
-                      <div className={clsx(styles.boxContent)}>
-                        <h3
-                          id='down-up'
+                <div className={clsx(styles.blockContent)}>
+                  <div className={clsx(styles.centerContent)}>
+                    <div className={clsx(styles.boxContent)}>
+                      <h3 id="down-up">{item.title}</h3>
+                      <p id="down-up">{item.subTitle}</p>
+                      <a href={item.url}>
+                        <div
+                          className={clsx(
+                            "col-xs-12 order-3 order-xl-4 no-default-spacing",
+                            styles.boxButton
+                          )}
+                          id="detail-btn-company"
                         >
-                          {item.title}
-                        </h3>
-                        <p
-                          id='down-up'
-                        >
-                          {item.subTitle}
-                        </p>
-                        <a href={item.url}>
-                          <div
-                            className={clsx(
-                              "col-xs-12 order-3 order-xl-4 no-default-spacing",
-                              styles.boxButton
-                            )}
-                            id="detail-btn-company"
-                          >
-                            <span id="detail-btn-company-content">
-                              {item.btnContent}
-                            </span>
-                            <svg id="stroke-arr-btn" viewBox="0 0 64 7">
-                              <path d="M0 6h61.5l-5.2-5.2"></path>
-                            </svg>
-                          </div>
-                        </a>
-                      </div>
+                          <span id="detail-btn-company-content">
+                            {item.btnContent}
+                          </span>
+                          <svg id="stroke-arr-btn" viewBox="0 0 64 7">
+                            <path d="M0 6h61.5l-5.2-5.2"></path>
+                          </svg>
+                        </div>
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </SwiperSlide>
+          </div>
         ))}
-      </Swiper>
+      </div>
     </>
   );
 };
--- napa-solution/pages/index.js
+++ napa-solution/pages/index.js
@@ -3,74 +3,14 @@ import { convertArrToObject } from "../util/converArrayToObject";
 import Company from "../components/homepage/Company";
 import Head from "next/head";
 import Begin from "../components/homepage/Begin";
-import News from "../components/homepage/News/index.js";
 import Service from "../components/homepage/Service/index.js";
 import Project from "../components/homepage/Project/index.js";
 import ClientSay from "../components/homepage/ClientSay/index.js";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Mousewheel } from "swiper";
 import { client } from "../apolo-client";
 
-import "swiper/swiper.min.css";
-import { useEffect, useRef, useState } from "react";
-
-SwiperCore.use([Mousewheel]);
-
 const Index = ({ footer, data, ...props }) => {
-  const [swiperRef, setSwiperRef] = useState(null);
-  const onTopBtnRef = useRef(null);
-  let headerRef = useRef(null);
-
   const datas = convertArrToObject(data.page.layouts);
   const clientSay = data.clientSay;
-
-  useEffect(() => {
-    if (swiperRef) {
-      swiperRef.update();
-      swiperRef.updateProgress();
-      swiperRef.updateSize();
-      swiperRef.updateSlides();
-      onTopBtnRef.current.classList.add("ot-container-nonvisible");
-    }
-    headerRef.current = document.getElementById("navbar");
-
-    // console.log("header", headerRef);
-  }, [swiperRef, setSwiperRef]);
-
-  const scrollToTop = () => {
-    if (swiperRef) {
-      swiperRef.slideTo(0);
-    }
-  };
-
-  const handleSlideChange = (sw) => {
-    // console.log(sw.activeIndex, headerRef.current);
-
-    // toogle visible on top button
-    if (onTopBtnRef.current) {
-      if (sw.activeIndex > 0) {
-        onTopBtnRef.current.classList.remove("ot-container-nonvisible");
-      } else {
-        onTopBtnRef.current.classList.add("ot-container-nonvisible");
-      }
-    }
-
-    if (headerRef.current) {
-      // toogle color tranparent
-      if (sw.activeIndex <= 0) {
-        headerRef.current.classList.remove("dark-nav");
-      } else {
-        headerRef.current.classList.add("dark-nav");
-      }
-
-      // toogle visible nav bar
-      if (sw.activeIndex == 1) {
-        headerRef.current.classList.add("navbar-hidden-force");
-      } else {
-        headerRef.current.classList.remove("navbar-hidden-force");
-      }
-    }
-  };
 
   return (
     <>
@@ -107,71 +47,19 @@ const Index = ({ footer, data, ...props }) => {
 
         {/* <link rel="stylesheet" href="/node_modules/swiper/swiper.scss" /> */}
       </Head>
-      <Swiper
-        className="sw-container "
-        onSwiper={setSwiperRef}
-        onSlideChange={handleSlideChange}
-        direction={"vertical"}
-        slidesPerView={1}
-        mousewheel
-      >
-        <SwiperSlide className="sw-slide-all-page ">
-          <Begin data={data.banner} />
-        </SwiperSlide>
-        <SwiperSlide className="sw-slide-all-page ">
-          <Service data={datas["Service"]} />
-        </SwiperSlide>
-        <SwiperSlide className="sw-slide-all-page ">
-          <Swiper
-            className="sw-container"
-            direction={"vertical"}
-            slidesPerView={"auto"}
-            mousewheel
-            freeMode
-            nested
-            onScroll={(sw, e) => sw.update()}
-            onTouchStart={(sw, e) => sw.update()}
-          >
-            <SwiperSlide className="sw-slide-free-mode ">
-              <div
-                id="root"
-                className="container-fluid content-wrapper no-default-spacing"
-              >
-                {/* <News data={data["Home_News"]} /> */}
 
-                <Company data={datas["Company"]} />
-                <Project data={datas["Slides_Section"]} />
-                <ClientSay data={clientSay} />
-              </div>
-              {footer}
-            </SwiperSlide>
-          </Swiper>
-        </SwiperSlide>
-      </Swiper>
-      {/* <div ref={onTopBtnRef} className="ot-container" onClick={scrollToTop}>
-        <img className="ot-image" src="./img/scroll-top.png" />
-      </div> */}
-      <div
-        ref={onTopBtnRef}
-        className="scroll_to_top "
-        id="scrollToTopSlide"
-        onClick={scrollToTop}
-      >
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="arrow-up"
-          className="scroll-top-img svg-inline--fa fa-arrow-up fa-w-14"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
+      <div className="">
+        <Begin data={data.banner} />
+        <Service data={datas["Service"]} />
+        <div
+          id="root"
+          className="container-fluid content-wrapper no-default-spacing"
         >
-          <path
-            fill="currentColor"
-            d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
-          ></path>
-        </svg>
+          <Company data={datas["Company"]} />
+          <Project data={datas["Slides_Section"]} />
+          <ClientSay data={clientSay} />
+        </div>
+        {footer}
       </div>
     </>
   );
