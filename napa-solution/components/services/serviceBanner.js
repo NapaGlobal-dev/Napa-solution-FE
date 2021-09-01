e0df85627d155ee251a4637ce1c07aaacdaf208b
DungxDang <dung.ddang@gmail.com>
2021-09-01 11:50:36 +0700

    change banner case study ui

--- napa-solution/components/services/serviceBanner.js
+++ napa-solution/components/services/serviceBanner.js
@@ -1,14 +1,8 @@
-import { getData } from "../../util/converArrayToObject";
 import Head from "next/head";
 import { useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function Banner(props) {
-  // const banner = getData(data, /ServicesWeb_Banner_Img/)[0];
-  // const title = getData(data, /ServicesWeb_Banner_Title/)[0];
-  // const subtitle = getData(data, /ServicesWeb_Banner_SubTitle/)[0];
-  // const content = getData(data, /ServicesWeb_Banner_Content/)[0];
-  // const subcontent = getData(data, /ServicesWeb_Banner_SubContent/)[0];
   const { banner } = props;
   // useEffect(() => {
   //   window.onload = function () {
@@ -23,19 +17,20 @@ export default function Banner(props) {
       <div className="banner-page banner new-version">
         <div className="title">
           <h1 className="main-title animate-down-up">
-            {banner?.Title?.key}
             {banner?.Title?.value}
           </h1>
           <div className="sub-menu animate-down-up">
-            {banner?.SubTitle?.key}
             {banner?.SubTitle?.value}
           </div>
         </div>
         <div className="img-banner animate-down-up">
-          <img
+          <LazyLoadImage
+            effect='blur'
             src={banner?.Img?.image?.original}
+            placeholderSrc={banner?.Img?.image?.thumbnail}
+            threshold={100}
+            width='100%'
             className="image-banner"
-            alt=""
           />
         </div>
         <img className="shape-banner" src="/img/wave-shape.svg" />
