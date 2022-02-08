753ba26fdd10c4564ec4475f50cf5be5c82d26e5
Nguyễn Tuấn Quang Sang <43717681+sinhvienit17@users.noreply.github.com>
2022-02-08 15:37:14 +0700

    Update config.js

--- napa-solution/config.js
+++ napa-solution/config.js
@@ -4,7 +4,8 @@ const HOST = process.env.NEXT_PUBLIC_API || "localhost";
 const PORT = "3001";
 
 const apiEnv = {
-  production: `https://api.napa-solutions.com/admin/api`,
+  //production: `https://api.napa-solutions.com/admin/api`,
+  production: "http://13.212.4.35:3001/admin/api",
   dev: `http://${HOST}:${PORT}/admin/api`,
 };
 
