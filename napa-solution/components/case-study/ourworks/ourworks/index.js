d0ab7248695c1a534c76da0ae488131d65814ebf
quangviet93 <82695332+quangviet93@users.noreply.github.com>
2022-05-30 11:22:44 +0700

    fix css contact,clear item JODC and Mobile Web App CaseStudies

--- napa-solution/components/case-study/ourworks/ourworks/index.js
+++ napa-solution/components/case-study/ourworks/ourworks/index.js
@@ -29,7 +29,10 @@ function OurWork({ data, service }) {
     },
   ];
   const keys = arrKeys.concat(
-    data?.keys[0]?.content && data?.keys[0]?.content.slice()
+    data?.keys[0]?.content &&
+      data?.keys[0]?.content.slice().filter((e) => {
+        return e.key !== "JODC" && e.key !== "Mobile Application Development";
+      })
   );
   const title = data?.caseStudies[0]?.key;
   const subTitle = data?.caseStudies[0]?.value;
@@ -39,7 +42,6 @@ function OurWork({ data, service }) {
       ?.map((value) => ({ value, sort: Math.random() }))
       .sort((a, b) => a.sort - b.sort)
       .map(({ value }) => value);
-      console.log(tempList)
     setCaseStudies(
       tempList?.filter((e) => {
         return (
@@ -48,7 +50,8 @@ function OurWork({ data, service }) {
           e.value !== "Hackathon" &&
           e.value !== "Ultorex"
         );
-      }) || []);
+      }) || []
+    );
   }, [data]);
 
   const caseStudyList = (() => {
@@ -86,8 +89,6 @@ function OurWork({ data, service }) {
     if (e.target.innerHTML == "COLLAPSE") setLoadmore(6);
     else setLoadmore(loadmore + 6);
   };
-
-  console.log('caseStudiesList', caseStudyList)
 
   return (
     <>
@@ -136,10 +137,10 @@ function OurWork({ data, service }) {
                 caseStudyList?.length > 6
                   ? loadmore < 6
                     ? clsx(
-                      styles.wrapProjectRow,
-                      // styles.fixheight,
-                      styles.loadmore
-                    )
+                        styles.wrapProjectRow,
+                        // styles.fixheight,
+                        styles.loadmore
+                      )
                     : clsx(styles.wrapProjectRow)
                   : clsx(styles.wrapProjectRow)
               }
@@ -148,8 +149,8 @@ function OurWork({ data, service }) {
                 <Row
                   entry={entry}
                   key={entry.name}
-                // loading={loadingProject}
-                // onClick={() => router.push(`${entry.url}`)}
+                  // loading={loadingProject}
+                  // onClick={() => router.push(`${entry.url}`)}
                 />
               ))}
             </div>
