12d398fe4fa429438973977c4686947b785bb8f1
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2024-02-15 14:53:16 +0700

    config seo preview v6

--- napa-solution/pages/index.js
+++ napa-solution/pages/index.js
@@ -52,6 +52,7 @@ const Index = ({ footer, data, ...props }) => {
 				/>
 
 				{/* <link rel="stylesheet" href="/node_modules/swiper/swiper.scss" /> */}
+				<meta property="og:url" content="https://napa-solutions.com/" />
 				<meta
 					property="og:image"
 					content="https://napa-website-data.s3.ap-southeast-1.amazonaws.com/NapaSolutions/seo-preview-image-300x300.png"
